31b41123fd504a23a92091fb5f8b96167d199d42
Charles A. Hartman <charles@hartman.dev>
2020-10-17 11:21:22 -0700

    housecleaning

--- other_resources/polymorhism.rb
+++ other_resources/polymorhism.rb
@@ -1,30 +1,25 @@
 # polymorhism.rb
 
 class Animal
+  def make_noise
+    'Some noise'
+  end
 
-    def make_noise
-        "Some noise"
-    end
-
-    def sleep
-        puts "#{self.class.name} is sleeping."
-    end
-
+  def sleep
+    puts "#{self.class.name} is sleeping."
+  end
 end
 
 class Dog < Animal
-
-    def make_noise
-        'Woof!'
-    end
-
+  def make_noise
+    'Woof!'
+  end
 end
 
 class Cat < Animal
-
-    def make_noise
-        'Meow!'
-    end
+  def make_noise
+    'Meow!'
+  end
 end
 
 [Animal.new, Dog.new, Cat.new].each do |animal|
