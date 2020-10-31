43580f367c8943a23ae99ad7b5782aefb25afc96
Charles A. Hartman <charles@hartman.dev>
2020-10-30 20:50:14 -0700

    begin assessment prep RB129

---
+++ assessment_prep/gallery_transaction_p1.rb
@@ -0,0 +1,16 @@
+# gallery_transaction_p1.rb
+
+module Displayable; end
+
+class Transaction; end
+
+class Client; end
+
+class Artwork; end
+
+class Painting < Artwork; end
+
+class Photograph < Artwork; end
+
+class Artist; end
+
